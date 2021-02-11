66773feefc8417696c291cc58659e7d7527030ba
aaliakinci <aliakiinci@gmail.com>
2021-02-11 20:57:09 +0300

    theme changer improvoment language

--- src/Components/ThemeChanger/index.js
+++ src/Components/ThemeChanger/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useContext, useState } from 'react';
 import ThemeContext from '../../Context/ThemeContext';
 import Switch from '../../../node_modules/@material-ui/core/Switch/Switch';
+import { FormattedMessage } from 'react-intl';
 function ThemeChanger() {
 	const { theme, setTheme } = useContext(ThemeContext);
 	const [changerTheme, setChangerTheme] = useState({
@@ -20,7 +21,7 @@ function ThemeChanger() {
 		<div className="row">
 			<div className="col-12 d-flex justify-content-between align-items-center">
 				<div className="form-group">
-					<label className={`${theme==="light"?"text-dark":"text-white"}`} >{theme === 'light' ? 'Açık Tema' : 'Koyu Tema'}</label>
+					<label className={`${theme==="light"?"text-dark":"text-white"}`} >{theme === 'light' ? <FormattedMessage id="change.light"/> :<FormattedMessage id="change.dark"/> }</label>
 					<Switch
 						checked={changerTheme.checkedA}
 						onChange={handleChange}
--- src/Language/index.js
+++ src/Language/index.js
@@ -25,7 +25,9 @@ export const language = {
 		'home.dummyTextHeaderFirst':"Lorem Ipsum Nedir?",
 		'home.dummyTextFirst':"Lorem Ipsum,dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.Lorem Ipsum,adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
 		'home.dummyTextHeaderSecond':"Neden Kullanırız?",
-		'home.dummyTextSecond':"Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir."
+		'home.dummyTextSecond':"Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.",
+		'change.light':"Açık Tema",
+		'change.dark':"Koyu Tema",
 	},
 	'en-US': {
 		'header.brand': 'Who am I ?',
@@ -57,5 +59,7 @@ export const language = {
 		'home.dummyTextHeaderSecond': 'Why do we use it?',
 		'home.dummyTextSecond':
 			"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+			'change.light':"Light Theme",
+		'change.dark':"Dark Theme",
 	},
 };
