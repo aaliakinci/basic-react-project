b2e9b58414b8c025ab4289e0fe39fd1fed456fc8
aaliakinci <aliakiinci@gmail.com>
2021-02-11 20:22:37 +0300

    searchbar improvoment

--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -1,10 +1,10 @@
-import { useState, useEffect,useContext} from 'react';
+import { useState, useEffect, useContext } from 'react';
 import ThemeContext from '../../Context/ThemeContext';
 import styles from './styles.module.css';
-function SearchBar({ setSelectedCountry, data,setData,contactData}) {
+function SearchBar({ setSelectedCountry, data, setData, contactData }) {
 	const [search, setSearch] = useState('');
 	const [filterCountry, setFilterCountry] = useState([]);
-	const {theme} = useContext(ThemeContext);
+	const { theme } = useContext(ThemeContext);
 	useEffect(() => {
 		const fetchData = async () => {
 			setFilterCountry(
@@ -14,33 +14,41 @@ function SearchBar({ setSelectedCountry, data,setData,contactData}) {
 			);
 		};
 		fetchData();
-		if (filterCountry.length > 0) {
-			if (filterCountry.length < 2 && search === filterCountry[0].name) {
-				setSelectedCountry(filterCountry[0].id);
-				setFilterCountry([]);
+		const changeFilter = () => {
+			if (filterCountry.length > 0) {
+				if (filterCountry.length < 2 && search === filterCountry[0].name) {
+					setSelectedCountry(filterCountry[0].id);
+					setData({ ...contactData, country: filterCountry[0].id });
+					setFilterCountry([]);
+				}
 			}
 		}
+		changeFilter();
 	}, [search,data,setSelectedCountry]);
 	const handleChange = (e) => {
 		setSearch(e.target.value);
 	};
 
- const handleClick = (country) => {
-	setSelectedCountry(country.id)
-	setData({...contactData,country:country.id})
-	const search = document.getElementById('search');
-	search.value=country.name
-	setFilterCountry([]);
- }
+	const handleClick = (country) => {
+		setSelectedCountry(country.id);
+		setData({ ...contactData, country: country.id });
+		const search = document.getElementById('search');
+		search.value = country.name;
+		setFilterCountry([]);
+	};
 	return (
 		<div className="form-group">
-			<input id="search" className="form-control"  onChange={handleChange} />
+			<input id="search" autoComplete="off" className="form-control" onChange={handleChange} />
 			{filterCountry.length < 200 ? (
 				<ul id="countrys" className="list-group list-group-flush">
 					{filterCountry.map((country) => (
 						<li
-							className={`list-group-item ${theme==="light"?"":"text-white bg-transparent border border-top-0 border-left-0 border right-0"} ${styles.listItem}`}
-							onClick={()=>handleClick(country)}
+							className={`list-group-item ${
+								theme === 'light'
+									? ''
+									: 'text-white bg-transparent border border-top-0 border-left-0 border right-0'
+							} ${styles.listItem}`}
+							onClick={() => handleClick(country)}
 							key={country.id}
 						>
 							{country.name}
