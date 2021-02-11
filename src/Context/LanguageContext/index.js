895c34fa0ccf91bacb579d9b17d664071309b092
aaliakinci <aliakiinci@gmail.com>
2021-02-11 19:12:25 +0300

    readme update

--- src/Context/LanguageContext/index.js
+++ src/Context/LanguageContext/index.js
@@ -1,10 +1,25 @@
-import {createContext,useState} from 'react';
+import {createContext,useState,useEffect} from 'react';
 
 const LanguageContext = createContext(null);
 
 export const LanguageProvider = ({children}) => {
 	const defaultLang = navigator.language
 	const [lang, setLang] = useState(defaultLang)
+
+	// useEffect(() => {
+	// 	const isLocal = localStorage.getItem('lang');
+	// 	console.log(isLocal);
+	// 	if(isLocal===undefined)
+	// 	{
+	// 		const defaultLang = navigator.language
+	// 		setLang(defaultLang)
+	// 		localStorage.setItem('lang',lang);
+	// 	}
+	// 	else{
+	// 		localStorage.setItem('lang',lang)
+	// 	}
+	// }, [lang])
+
 
 	const values = {
 		lang,
